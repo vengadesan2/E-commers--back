d5127736cc7f84f0cead512029f55da6739784f4
vengadesan2 <vengadesanguna24@gmail.com>
2024-07-26 12:25:45 +0530

    updates1

--- config/database.js
+++ config/database.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const connectDatabase = ()=>{
-    mongoose.connect(process.env.DB_LOCAL_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(con=>{
+    mongoose.connect(process.env.DB_LOCAL_URI).then(con=>{
         console.log(`MongoDB is connected to the host: ${con.connection.host} `)
     })
 }
